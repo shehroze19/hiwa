4d528ed6c29dc508454318d6bbb51515c570758e
shehroze19 <44766912+shehroze19@users.noreply.github.com>
2019-05-10 01:28:00 -0400

    image check for logo and added mysql_real_escape_string and htmlentities for validation and cookie>session

--- php/products.php
+++ php/products.php
@@ -2,31 +2,34 @@
 require 'config.phplib';
 
 $msg="";
-if (!array_key_exists('hiwa-user', $_COOKIE) ||
-    !array_key_exists('hiwa-role', $_COOKIE)) {
+if (!isset($_SESSION['hiwa-user'])) || (!isset($_SESSION['hiwa-role'])) {
 	Header("Location: login.php");
 	exit();
 }
 
-$role=$_COOKIE['hiwa-role'];
+$role=$_SESSION['hiwa-role'];
 
 $nextAction = "blank";
 if (array_key_exists('action', $_REQUEST) && array_key_exists('prodid', $_REQUEST)) {
 	if ($_REQUEST['action'] == 'delete') {
 		$conn = pg_connect('user='.$CONFIG['username'].
 			' dbname='.$CONFIG['database']);
-		$res = pg_query($conn, "DELETE FROM products WHERE 
-			productid='".$_REQUEST['prodid']."'");
-		if ($res === FALSE) {
+		$retrieved_id=(int)$_REQUEST['prodid'];// making sure the id is in integer
+		// using prepare statemnt for validation of input
+		$res = pg_prepare($conn, "query_runner", "DELETE FROM products WHERE 
+			productid='".$retrieved_id."'");
+		$result=pg_execute($conn,"query_runner"); // executing 
+		if ($result === FALSE) {
 			$msg = "Unable to remove customer";
 		}
 	} else if ($_REQUEST['action'] == 'edit') {
 		$nextAction = "update";
 		$conn = pg_connect('user='.$CONFIG['username'].
 			' dbname='.$CONFIG['database']);
-		$res = pg_query("select productid,productname,productdescr,msrp,imageurl from products where productid='".
-			$_REQUEST['prodid']."'");
-		$cache = pg_fetch_assoc($res);
+		$res = pg_prepare($conn,"edit_statement","select productid,productname,productdescr,msrp,imageurl from products where productid='".
+			$retrieved_id."'");
+		$result=pg_execute($conn,"edit_statement"); // executing 
+		$cache = pg_fetch_assoc($result);
 		pg_free_result($res);
 		pg_close($conn);
 	}
@@ -36,28 +39,30 @@ if (array_key_exists("a", $_REQUEST)) {
 	if ($_REQUEST['a'] == 'Add Product') {
 		if ($_FILES['prodimg']['tmp_name'] != "") {
 			$imgname=$_FILES['prodimg']['name'];
-			if (mime_content_type($_FILES['prodimg']['tmp_name']) != 'text/x-php')
+			if ($imgname!='hiva.png' || mime_content_type($_FILES['prodimg']['tmp_name']) != 'text/x-php') // if same name then it wont work
 			copy($_FILES['prodimg']['tmp_name'],
 				$CONFIG['uploads'].'/'.$_FILES['prodimg']['name']);
 		} else {
 			$imgname='';
 		}
-			
+			// adding htmlentties so that no xss and real escape for validation of input for '
 		$conn = pg_connect('user='.$CONFIG['username'].
 			' dbname='.$CONFIG['database']);
-		$res = pg_query($conn, "INSERT INTO products
+		$res = pg_prepare($conn, "insert_query", "INSERT INTO products
 			(productid, productname, productdescr, msrp, imageurl)
 			VALUES
-			('".$_REQUEST['prodid']."', '".
-			$_REQUEST['prodname']."', ".
-			"'".$_REQUEST['proddesc']."', ".
-			$_REQUEST['msrp'].", ".
+			('".mysql_real_escape_string(htmlentities($_REQUEST['prodid']))."', '".
+			mysql_real_escape_string(htmlentities($_REQUEST['prodname']))."', ".
+			"'".mysql_real_escape_string(htmlentities($_REQUEST['proddesc']))."', ".
+			mysql_real_escape_string(htmlentities($_REQUEST['msrp'])).", ".
 			"'".$imgname."');");
-		if ($res === FALSE) {
+		
+		$result=pg_execute($conn,"insert_query"); // executing 
+		if ($result === FALSE) {
 			$msg="Unable to create product.";
 		}
 	} elseif ($_REQUEST['a'] == 'Update product') {
-		if ($_FILES['prodimg']['tmp_name'] != "") {
+		if ($_FILES['prodimg']['tmp_name'] != "" || $imgname!='hiva.png' ) { // should not have icons name
 			$imgname=$_FILES['prodimg']['name'];
 			copy($_FILES['prodimg']['tmp_name'],
 				$CONFIG['uploads'].'/'.$_FILES['prodimg']['name']);
@@ -66,13 +71,13 @@ if (array_key_exists("a", $_REQUEST)) {
 		}
 		$conn = pg_connect('user='.$CONFIG['username'].
 			' dbname='.$CONFIG['database']);
-		$res = pg_query($conn, "update products ".
-			"set productname='".$_REQUEST['prodname']."',".
-			"    productdescr='".$_REQUEST['proddesc']."',".
-			"    msrp=".$_REQUEST['msrp'].",".
+		$res = pg_prepare($conn, "update_query", "update products ".
+			"set productname='".mysql_real_escape_string(htmlentities($_REQUEST['prodname']))."',".
+			"    productdescr='".mysql_real_escape_string(htmlentities($_REQUEST['proddesc']))."',".
+			"    msrp=".mysql_real_escape_string(htmlentities($_REQUEST['msrp'])).",".
 			"    imageurl='".$imgname."'".
 			"where productid='".$_REQUEST['prodid']."'");
-		$res = pg_query($conn, "commit;");
+		$res = pg_execute($conn,"insert_query"); // executing 
 		if ($res === FALSE) {
 			$msg="Unable to update product.";
 		}
@@ -90,7 +95,7 @@ if (array_key_exists("a", $_REQUEST)) {
 <body>
 <?php require 'header.php';?>
 <div class="title">HIWA Manage Products</div>
-<div class="subtitle">Logged in as <?php echo $_COOKIE['hiwa-user'];?>(<?php 
+<div class="subtitle">Logged in as <?php echo $_SESSION['hiwa-user'];?>(<?php 
 	echo $role; ?>)
 </div>
 
